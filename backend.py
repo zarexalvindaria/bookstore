87aee4b94be400f63b6df2185f284d478c40f366
zarexalvindaria <zarexalvindaria@users.noreply.github.com>
2021-07-28 13:50:47 +0800

    added the frontend functions

--- backend.py
+++ backend.py
@@ -1,10 +1,13 @@
+"""Backend of the Bookstore"""
 import sqlite3
 
 
+# Define the backend functions
 def connect():
     connection = sqlite3.connect("bookstore.db")
     cursor = connection.cursor()
-    cursor.execute("CREATE TABLE IF NOT EXISTS books (id INTEGER PRIMARY KEY, title text, author text, year integer, isbn integer)")
+    cursor.execute(
+        "CREATE TABLE IF NOT EXISTS books (id INTEGER PRIMARY KEY, title text, author text, year integer, isbn integer)")
     connection.commit()
     connection.close()
 
@@ -12,7 +15,7 @@ def connect():
 def insert(title, author, year, isbn):
     connection = sqlite3.connect("bookstore.db")
     cursor = connection.cursor()
-    cursor.execute("INSERT INTO books VALUES (NULL,?,?,?,?)", (title,author,year,isbn))
+    cursor.execute("INSERT INTO books VALUES (NULL,?,?,?,?)", (title, author, year, isbn))
     connection.commit()
     connection.close()
 
@@ -29,10 +32,11 @@ def view():
 def search(title="", author="", year="", isbn=""):
     connection = sqlite3.connect("bookstore.db")
     cursor = connection.cursor()
-    cursor.execute("SELECT * FROM books WHERE title=? OR author=? OR year=? OR isbn=?", (title, author, year, isbn))
-    rows=cursor.fetchall()
+    cursor.execute("SELECT * FROM books WHERE title=? OR author=? OR year=? OR isbn=? AND (title IS NOT NULL AND author IS NOT NULL AND year IS NOT NULL AND isbn IS NOT NULL)", (title, author, year, isbn))
+    rows = cursor.fetchall()
     connection.close()
     return rows
+
 
 def delete(id):
     connection = sqlite3.connect("bookstore.db")
@@ -41,16 +45,21 @@ def delete(id):
     connection.commit()
     connection.close()
 
+
 def update(id, title, author, year, isbn):
     connection = sqlite3.connect("bookstore.db")
     cursor = connection.cursor()
-    cursor.execute("UPDATE books SET title=?, author=?, year=?, isbn=? WHERE id=?", (id, title, author, year, isbn))
+    cursor.execute("UPDATE books SET title=?, author=?, year=?, isbn=? WHERE id=?", (title, author, year, isbn, id))
     connection.commit()
     connection.close()
 
 
 connect()
 # insert("Mathematics","John Doe",1993,12345)
+# insert("Science","John Doe",1993,12345)
+# insert("English","John Doe",1993,12345)
 # print(search("","",1993))
 # delete(1)
+# print(view())
+# update(3, "English", "John Smith", 1990, 646646)
 # print(view())
--- frontend.py
+++ frontend.py
@@ -14,13 +14,13 @@ Close
 """
 
 import tkinter as tk
-from tkinter import StringVar
+from tkinter import StringVar, END
 import backend
 
 root = tk.Tk()
 root.title("Book Store")
-# root.geometry("400x200")
 root.iconbitmap("book.ico")
+root.geometry("350x230")
 root.resizable(0,0)
 
 # Define colors and fonts
@@ -30,6 +30,64 @@ dark_red = "#a80a0d"
 gray = "#4b4b4b"
 
 
+# Define functions
+
+def get_selected_row(event):
+    """Get the id of the selected row"""
+    global selected_tuple
+    index = book_list.curselection()[0]
+    selected_tuple = book_list.get(index)
+    title_entry.delete(0, END)
+    title_entry.insert(END, selected_tuple[1])
+    author_entry.delete(0, END)
+    author_entry.insert(END, selected_tuple[2])
+    year_entry.delete(0, END)
+    year_entry.insert(END, selected_tuple[3])
+    isbn_entry.delete(0, END)
+    isbn_entry.insert(END, selected_tuple[4])
+
+
+def view_command():
+    book_list.delete(0, END)
+    for row in backend.view():
+        book_list.insert(END, row)
+
+
+def search_command():
+    book_list.delete(0, END)
+    for row in backend.search(title_text.get(), author_text.get(), year_text.get(), isbn_text.get()):
+        book_list.insert(END, row)
+
+
+def add_command():
+    backend.insert(title_text.get(), author_text.get(), year_text.get(), isbn_text.get())
+    clear_list()
+    book_list.insert(END, (title_text.get(), author_text.get(), year_text.get(), isbn_text.get()))
+    # clear_input()
+
+
+def delete_command():
+    backend.delete(selected_tuple[0])
+    update_listview()
+
+
+def clear_list():
+    book_list.delete(0, END)
+
+
+def clear_input():
+    title_text.set("")
+    author_text.set("")
+    year_text.set("")
+    isbn_text.set("")
+
+
+def update_listview():
+    book_list.delete(0, END)
+    for row in backend.view():
+        book_list.insert(END, row)
+
+
 # GUI Layout
 # Define frames
 entry_frame = tk.LabelFrame(root)
@@ -37,7 +95,7 @@ list_frame = tk.LabelFrame(root)
 entry_frame.pack(padx=1, pady=5)
 list_frame.pack(ipady=5)
 
-
+# Define entry frame
 # First row
 title_label = tk.Label(entry_frame, text="Title", padx=5)
 title_label.grid(row=0, column=0)
@@ -61,27 +119,31 @@ isbn_text = StringVar()
 isbn_entry = tk.Entry(entry_frame, textvariable=isbn_text)
 isbn_entry.grid(row=1, column=3)
 
+# Define display
 book_list = tk.Listbox(list_frame, height=8, width=35)
 book_list.grid(row=2, column=0, rowspan=8, columnspan=2, padx=5)
 
 scrollbar = tk.Scrollbar(list_frame)
-scrollbar.grid(row=2, column=2, rowspan=6)
+scrollbar.grid(row=2, column=2, rowspan=6, sticky="NS")
 
 book_list.configure(yscrollcommand=scrollbar.set)
 scrollbar.configure(command=book_list.yview)
 
+# Define the binding of the Listbox and the
+book_list.bind("<<ListboxSelect>>", get_selected_row)
+
 # Define buttons
-view_button = tk.Button(list_frame, text="View all", width=12)
+view_button = tk.Button(list_frame, text="View all", width=12, command=view_command)
 view_button.grid(row=2, column=3)
-search_button = tk.Button(list_frame, text="Search entry", width=12)
+search_button = tk.Button(list_frame, text="Search entry", width=12, command=search_command)
 search_button.grid(row=3, column=3)
-add_button = tk.Button(list_frame, text="Add entry", width=12)
+add_button = tk.Button(list_frame, text="Add entry", width=12, command=add_command)
 add_button.grid(row=4, column=3)
-update_button = tk.Button(list_frame, text="Update", width=12)
+update_button = tk.Button(list_frame, text="Update", width=12, command=backend.update)
 update_button.grid(row=5, column=3)
-delete_button = tk.Button(list_frame, text="Delete", width=12)
+delete_button = tk.Button(list_frame, text="Delete", width=12, command=delete_command)
 delete_button.grid(row=6, column=3)
-close_button = tk.Button(list_frame, text="Close", width=12)
+close_button = tk.Button(list_frame, text="Close", width=12, command=root.destroy)
 close_button.grid(row=7, column=3)
 
 root.mainloop()
